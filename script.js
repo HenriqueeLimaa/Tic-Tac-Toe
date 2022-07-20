f3ae2d51056ab049cc9eae0d8035afddbd971852
HenriqueeLimaa <henrique.lima@pucrs.br>
2022-07-20 11:52:26 -0300

    Switch beetween player1 and player2 to play

--- script.js
+++ script.js
@@ -1,61 +1,78 @@
-const Gameboard = (() => { 
-    let _gameboardArray = ["", "", "", "", "", "", "", "", ""];
+const Gameboard = (() => {
+  let _gameboardArray = ["", "", "", "", "", "", "", "", ""];
 
-    const getArrayPosition = (position) => {
-        return _gameboardArray[position];
+  const getArrayPosition = (position) => {
+    return _gameboardArray[position];
+  };
+
+  const getArrayLength = () => {
+    return _gameboardArray.length;
+  };
+
+  const addPieceIntoArray = (position, symbol) => {
+    if (position < getArrayLength()) {
+      _gameboardArray.splice(position, 1, symbol);
     }
+  };
 
-    const getArrayLength = () => {
-        return _gameboardArray.length;
-    }
-
-    const addPieceIntoArray = (position, symbol) => {
-        if(position < getArrayLength()){
-            _gameboardArray.splice(position, 1, symbol);
-        }
-    }
-
-    return {getArrayPosition, getArrayLength, addPieceIntoArray};
+  return { getArrayPosition, getArrayLength, addPieceIntoArray };
 })();
 
 ////////////
 
 const playerFactory = (name, symbol) => {
-    const getName = () => {return name};
-    const getSymbol = () => {return symbol};
+  const getName = () => {
+    return name;
+  };
+  const getSymbol = () => {
+    return symbol;
+  };
 
-    return {getName, getSymbol};
-}
+  return { getName, getSymbol };
+};
 
 ///////////
 
-const GameFlow = (() => {
+const DisplayController = (() => {
+  const cells = document.querySelectorAll(".cell");
 
+  const displayIntoGameboard = (playerSymbol) => {
+    cells.forEach((cell) =>
+      cell.addEventListener("click", (event) => {
+        let cellClassName = cell.className.slice(-1);
+        Gameboard.addPieceIntoArray(cellClassName, playerSymbol);
+        addPieceToGameboard();
+      })
+    );
+  };
 
+  const addPieceToGameboard = () => {
+    for (let i = 0; i < Gameboard.getArrayLength(); i++) {
+      if (Gameboard.getArrayPosition(i) !== "") {
+        cells[i].textContent = Gameboard.getArrayPosition(i);
+      }
+    }
+  };
+
+  return { cells, addPieceToGameboard, displayIntoGameboard };
 })();
-
 
 //////////
 
-const DisplayController = (() => {
+const GameFlow = (() => {
+  let i = 0;
 
-    const _cells = document.querySelectorAll('.cell');
+  const player1 = playerFactory("p1", "X");
+  const player2 = playerFactory("p2", "O");
 
-    _cells.forEach(cell => cell.addEventListener('click', (event) => {
-        console.log(Gameboard.getArrayLength());
-        let cellClassName = cell.className.slice(-1);
-        Gameboard.addPieceIntoArray(cellClassName, 'X');
-        addPieceToGameboard();
-    }))
-
-    const addPieceToGameboard = () => {
-        for(let i=0;i<Gameboard.getArrayLength();i++){
-            if(Gameboard.getArrayPosition(i) !== ""){
-                console.log('aaa')
-                _cells[i].textContent = Gameboard.getArrayPosition(i);
-            }
-        }
-    }
-
-    return {addPieceToGameboard};
+  DisplayController.cells.forEach((cell) =>
+    addEventListener("click", () => {
+      if (i % 2 === 0) {
+        DisplayController.displayIntoGameboard(player1.getSymbol());
+      } else {
+        DisplayController.displayIntoGameboard(player2.getSymbol());
+      }
+      i++;
+    })
+  );
 })();
